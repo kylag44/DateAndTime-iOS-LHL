46d8d5d1f7ab3c6c175eb76b0f73018f87d5d1e9
steve thompson <steven.thompson.ca@gmail.com>
2017-12-18 17:52:22 -0500

    updates

--- Date Time Programming.playground/Contents.swift
+++ Date Time Programming.playground/Contents.swift
@@ -3,39 +3,53 @@ import UIKit
 
 /*:
  # (NS)Date
+ - "A specific point in time, independent of any calendar or time zone."
  */
 
-
 let now = Date()
+
+//: Test to see which reference date Apple uses for Date
+
 now.timeIntervalSinceReferenceDate
 now.timeIntervalSince1970
 
-// This proves that Date uses Apple's reference date
+//: This proves that Date uses Apple's reference date
 
 Date(timeIntervalSinceReferenceDate: now.timeIntervalSinceReferenceDate)
 
-// https://www.epochconverter.com
+//: Create a date 10 minutes later
 
-// Create a date 10 minutes later
 let later = Date(timeIntervalSinceNow: 60 * 10)
-print(#line, later, later.timeIntervalSince1970)
+print(#line, later)
+print(#line, later.timeIntervalSinceReferenceDate)
 
 // Other operators are also available
 if later > now {
   print(#line, "Dates can be compared")
 }
 
-// var timeIntervalSinceNow: TimeInterval { get }
-// gives you the interval between some date and now, negative for the future
+/*:
+ ```swift
+ var timeIntervalSinceNow: TimeInterval { get }
+ ```
+ */
+
+//: timeIntervalSinceNow gives you the interval between some date and now, negative for the future.
+
 
 later.timeIntervalSinceNow
 
+//: TimeInterval is just a TypeAlias to double
+
 let interval = 60.0 as TimeInterval
+
+//: Add time intervals to Date
+
 let evenLater = later + interval // 11 mins in total
 
 /*:
  # DateComponents
- - DateComponents full initializer.
+ - DateComponents has a very long initializer, but everything has a default value so can be omitted.
  */
 
 /*
@@ -57,47 +71,58 @@ let evenLater = later + interval // 11 mins in total
  yearForWeekOfYear: nil)
  */
 
+//: You can just set/get its properties
+
 var finalDay = DateComponents()
 finalDay.calendar = Calendar(identifier: .gregorian)
 finalDay.timeZone = .current
-finalDay.day = 23
+finalDay.day = 22
 finalDay.month = 12
 finalDay.year = 2017
 finalDay.hour = 13
 finalDay.minute = 20
 
 /*:
- - Only set the components that are useful
- - ☢️ The system doesn't prevent you from setting nonsensical date component values
+ - Only set the components that are useful.
+ - ☢️ The system doesn't prevent you from setting nonsensical date component values.
  */
 
 var bumDate = DateComponents()
 bumDate.day = 31
 bumDate.month = 2
 
-// Getting the Date from components
+if let noDate = bumDate.date {
+  print(#line, noDate)
+}else {
+  print(#line, "date is nil because there is no Feb 31")
+}
+
+//: Getting the Date from components
 
 guard let dateValue = finalDay.date else { fatalError() }
+
+//: Getting the components
+if let day = finalDay.day, let month = finalDay.month {
+  print(#line, "finalDay is day: \(day), month: \(month)")
+}
 
 /*:
  # Calendar
  - Neither Date nor DateComponents know anything about the users current Calendar.
- - The main Calendar is the .gregorian, but there are others in wide use.
+ - The main Calendar in use internationally is the .gregorian calendar, but there are others which can make apps break!
  */
+
+//: Get the current one
 
 var calendar = Calendar.current
 
-/*:
- - Since the user could change the calendar while your app is loaded you can call:
- */
+//: Since the user could change the calendar while your app is loaded you can call:
 
 calendar = Calendar.autoupdatingCurrent
 
 guard let dateFromComponents = calendar.date(from: finalDay) else { fatalError() }
 
-/*:
- Converting from DateComponents to Date using Calendar
- */
+//: Converting from DateComponents to Date using Calendar
 
 var finalDay2 = DateComponents(year: 2017, month: 12, day: 22, hour: 18)
 let greg = Calendar(identifier: .gregorian)
@@ -106,17 +131,16 @@ let greg = Calendar(identifier: .gregorian)
 guard var dateFromComponents2 = greg.date(from: finalDay2) else { fatalError() }
 dateFromComponents2
 
-/*:
- You can set a specific time on an existing Date
- */
+//: You can set a specific time on an existing Date.
 
 guard let twentyMinsLater = greg.date(bySettingHour: 18, minute: 20, second: 30, of: dateFromComponents2) else { fatalError() }
 
 twentyMinsLater
 
-/*:
- Adding dates using Calendar (this is the correct way to add dates)
- */
+
+//:  Adding dates using Calendar.
+//: (This is the correct way to add dates. Why should you avoid creating dates by adding TimeIntervals to Date?)
+
 
 let calendar2 = Calendar.current
 let components2 = DateComponents(day: 1, hour: 1)
@@ -127,49 +151,50 @@ guard let date3 = calendar2.date(byAdding: components2, to: Date()) else {
 
 date3
 
-/*:
- Finding the next occurrence of a date
- */
+//: Finding the next occurrence of a date.
 
-let components3 = DateComponents(weekday: 2)
+let components3 = DateComponents(weekday: 2) // Monday
 guard let nextMonday = greg.nextDate(after: Date(), matching: components3, matchingPolicy: .nextTime) else {
   fatalError()
 }
 
 nextMonday
 
+//: More on DateInterval below!
+
 guard let nextWeekend = greg.nextWeekend(startingAfter: Date()) else { fatalError() }
 print(#line, nextWeekend)
 
 /*:
- Comparing Dates
- */
+ #### Comparing dates using Calendar and DateComponents
+*/
 
 let dayComponents = DateComponents(day:1)
-guard let tomorrowDate = Calendar.current.date(byAdding: dayComponents , to: Date()) else { fatalError() }
+guard let tomorrowDate = Calendar.current.date(byAdding: dayComponents, to: Date()) else { fatalError() }
 
-let tomorrow = Calendar.current.compare(Date() , to: tomorrowDate , toGranularity: .day)
+let tomorrow = Calendar.current.compare(Date(), to: tomorrowDate , toGranularity: .day)
 if tomorrow == ComparisonResult.orderedSame {
-  print(#line, "false on the same day")
+  print(#line, "if today and tomorrow are the same in terms of 1 day granularity.")
 } else if tomorrow == ComparisonResult.orderedAscending {
-  print(#line, "tomorrow is one day greater than today")
+  print(#line, "tomorrow is 1 day later than today, which it is.")
+} else if tomorrow == ComparisonResult.orderedDescending {
+  print(#line, "fires if tomorrow is earlier by 1 day than today, which it is not.")
 }
 
-// Simple Date Comparison
+//: Simple Date Comparison
 
 var components5 = DateComponents(hour: 3, minute: 10)
 guard let todayPlus2Hours = Calendar.current.date(byAdding: components5, to: Date()) else { fatalError() }
 
-// Start of Day
+//: Start of Day
 var startOfDay = Calendar.current.startOfDay(for: todayPlus2Hours)
 
 
-// End of Previous Day
+//: End of Previous Day (Should probably use DateComponents, see the questions).
 startOfDay -= 1 // subtract a second from day's beginning
 
 
-// Useful Date Comparisons
-
+//: Useful Date Comparisons
 
 Calendar.current.isDateInToday(Date())
 Calendar.current.isDateInWeekend(Date())
@@ -177,34 +202,32 @@ Calendar.current.isDateInTomorrow(Date())
 Calendar.current.isDateInYesterday(Date())
 
 /*:
- ## Date Formatting
- * (NS)DateFormatter is used to convert to and from a string representation of a date
+ # Date Formatting
+ * (NS)DateFormatter is used to convert to and from a string representation of a date.
  */
-
-// This is called a "Fixed Format Date" using the ISO 8601 standard. When would you want to use this and when would you not want to use this?
-
 
 var formatter1 = DateFormatter()
 formatter1.dateFormat = "yyyy-MM-dd HH:mm:ss"
 formatter1.timeZone = TimeZone(secondsFromGMT: 0)
 formatter1.string(from: Date())
 
+//: This is called a "Fixed Format Date" using the ISO 8601 standard. When would you want to use this and when would you not want to use this?
 
 
-// iOS 10+ uses ISO8601DateFormatter for Fixed Formats.
+//: iOS 10+ uses ISO8601DateFormatter for Fixed Formats. See the documentation.
 
-// Convenient way to create localized date strings
+//: Convenient way to create localized date strings (user facing).
 
 DateFormatter.localizedString(from: Date(), dateStyle: .full, timeStyle: .none)
 DateFormatter.localizedString(from: Date(), dateStyle: .none, timeStyle: .short)
 
-// Localized way of creating DateFormatter with full control
+//: Localized way of creating DateFormatter with full control
 
 var formatter2 = DateFormatter()
 formatter2.setLocalizedDateFormatFromTemplate("yyyy-MM-dd HH:mm:ss")
 formatter2.string(from: Date())
 
-// Converting Strings to Date (Used for network requests)
+//: Converting String dates to Date using template (Used for network requests).
 
 let formatter3 = DateFormatter()
 formatter3.dateFormat = "yyyy-MM-dd"
@@ -216,15 +239,17 @@ guard let newDate3 = formatter3.date(from: randomDateString) else {
 DateFormatter.localizedString(from: newDate3, dateStyle: .short, timeStyle: .none) // uses US Locale which is default on my system
 
 /*:
- ## DateInterval
+ # DateInterval
  * iOS 10+ Adds DateInterval.
- * DateInterval is a Swift only Struct that represents a 0 or positive time/date span.
+ * DateInterval is a Swift only Struct that represents a  positive time/date span or 0 (negatives are not supported).
  */
 
 let dateInterval = DateInterval(start: Date(), duration: 60*60*24)
 dateInterval.contains(Date())
 let endDatePlus1 = Date(timeInterval: 1, since: dateInterval.end)
 dateInterval.contains(endDatePlus1)
+
+//: Comparing DateInterval
 
 dateInterval < DateInterval(start: Date(), end: endDatePlus1)
 dateInterval > DateInterval(start: Date(), end: endDatePlus1)
@@ -234,9 +259,9 @@ dateInterval.duration
 86400/24/60/60
 
 /*:
- ### DateIntervalFormatter
+ # DateIntervalFormatter
  - "A formatter that creates string representations of time intervals."
- - For user readable time interval representations.
+ - For user readable date interval representations.
  */
 
 let intervalFormatter = DateIntervalFormatter()
@@ -258,7 +283,7 @@ Calendar.current.startOfDay(for: newDate6!)
 
 //3. Get end of the day 5 days from today
 
-
+//4. Using DateComponents get the Date/Time that is 1 week earlier and the very last second of the day.
 
 /*:
  ## References
@@ -268,6 +293,7 @@ Calendar.current.startOfDay(for: newDate6!)
  * [WWDC Session 227](https://developer.apple.com/videos/play/wwdc2013/227/)
  * [Online Fixed Format Tool](http://nsdateformatter.com)
  * [ISO 8601 Standard](https://en.wikipedia.org/wiki/ISO_8601)
+ * [Epoch Converter](https://www.epochconverter.com)
  */
 
 
